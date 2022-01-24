6e3b5f78bba15da0855bb2f4341ccd364ba1f214
Luis Guarda <luis.guarda.fernandez@outlook.com>
2022-01-24 15:56:24 -0500

    modificaciones de evaluación

--- src/main/java/com/bcp/pe/service/impl/MontoServiceImpl.java
+++ src/main/java/com/bcp/pe/service/impl/MontoServiceImpl.java
@@ -3,7 +3,15 @@ package com.bcp.pe.service.impl;
 import java.math.BigDecimal;
 import java.util.List;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
 import com.bcp.pe.beans.RequestMonto;
@@ -13,6 +21,9 @@ import com.bcp.pe.entity.TipoCambioEntity;
 import com.bcp.pe.repository.MontoRepository;
 import com.bcp.pe.repository.TipoCambioRepository;
 import com.bcp.pe.service.MontoService;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+
 
 import reactor.core.publisher.Mono;
 
@@ -36,6 +47,17 @@ public class MontoServiceImpl implements MontoService{
 		BigDecimal montoAcambiar=request.getMontoAcambiar();
 		
 		List<TipoCambioEntity> precioMoneda=tipoCambioRepository.findAll();
+		
+		//invocacion a api de tipos de cambio
+		
+		Client cliente= ClientBuilder.newClient();
+		
+		WebTarget webTarget=cliente.target("https://freecurrencyapi.net/api/v2/latest");
+		
+		Invocation.Builder solicitud = webTarget.request();
+		
+		Response response= solicitud.get();
+		
 
 		//Busqueda del precio de la moneda destino y operación
 		for(TipoCambioEntity i:precioMoneda) {
